290efac7465d377ffd520bd7925dfee8c166a146
Jamal <ejdoorson@gmail.com>
2021-11-04 10:09:11 +0100

    Created modules collectable, item, and villager. Added imports in app.module.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CollectableModule } from './collectable/collectable.module';
+import { ItemModule } from './item/item.module';
+import { VillagerModule } from './villager/villager.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CollectableModule,
+    ItemModule,
+    VillagerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
