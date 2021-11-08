bb5c765c5cd6cce30f646e1337cb7b98cc79715c
Jamal <ejdoorson@gmail.com>
2021-11-08 11:32:02 +0100

    Implemented bug list.

--- src/app/collectable/bug.ts
+++ src/app/collectable/bug.ts
@@ -1,3 +1,8 @@
 export interface Bug {
-    
+    id: number,
+    name: {
+        'name-USen': string,
+        'name-USes': string,
+    },
+    'catch-phrase': string,
 }
--- src/app/collectable/collectable-bug/collectable-bug.component.ts
+++ src/app/collectable/collectable-bug/collectable-bug.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Bug } from '../bug';
+import { CollectableService } from '../collectable.service';
 
 @Component({
   selector: 'app-collectable-bug',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collectable-bug.component.css']
 })
 export class CollectableBugComponent implements OnInit {
+  bugs: Bug[] | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.collectableService
+      .getBugs()
+      .subscribe(data => { this.bugs = Object.values(data) });
   }
 
+  constructor(private collectableService: CollectableService) { }
 }
